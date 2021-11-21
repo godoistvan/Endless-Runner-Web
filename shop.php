77b3fe4de76f066a7d6ff9ba135946b07d73a85b
godoistvan <godopisti@gmail.com>
2021-11-21 15:04:31 +0100

    Added packages
    Added the  packages to the website (not final version) coin will be replaced by custom image later in development

--- shop.php
+++ shop.php
@@ -13,35 +13,90 @@ require_once 'includes/navbarheader.php';
 
 <body>
   <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="..." alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">500  insert currency name </h5>
-            <p class="card-text">Buy 500 insert currency name  here for only 5$ and get 30 insert currency name extra</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">1000  insert currency name</h5>
-          <p class="card-text">Buy 1000 insert currency name  here for only 10$ and get 100 insert currency name extra.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">1000  insert currency name</h5>
-    <p class="card-text">Buy 1000 insert currency name  here for only 10$ and get 100 insert currency name extra.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <div class="row" style="min-height: 25px;"></div>
 
-    </div>
+    <!-- Az első sornyi termék amit eladunk -->
+    <div class="row">
+     
+    
+         <div class="col-3">
+                <div class="card">
+                    <img src="img/tempcoin.jpg" alt="#" style="min-height: 25vh; max-height: 25vh ;">
+                    <div class="card-body">
+                        <div style="min-height: 20vh; max-height: 20vh;"  >    
+                            <h5 class="card-title"></h5>
+                            <p class="blockquote">500  insert currency name </p>
+                            <p class="blockquote-footer">Buy 500 insert currency name  here for only 5$ and get 30 insert currency name extra</p>
+                        </div>
+                        <form action="includes/kosar.php" method="POST">
+                            <div class="input-group">
+                                <input type="number" name="darabszam" id="darabszam_input" class="form-control" value="1">
+                                <button type="submit" name="submit" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></button>
+                                <input type="hidden" name="termekID" value="">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/tempcoin.jpg" alt="#" style="min-height: 25vh; max-height: 25vh ;">
+                    <div class="card-body">
+                        <div style="min-height: 20vh; max-height: 20vh;"  >    
+                            <h5 class="card-title"></h5>
+                            <p class="blockquote">1000  insert currency name </p>
+                            <p class="blockquote-footer">Buy 1000 insert currency name  here for only 10$ and get 150 insert currency name extra</p>
+                        </div>
+                        <form action="includes/kosar.php" method="POST">
+                            <div class="input-group">
+                                <input type="number" name="darabszam" id="darabszam_input" class="form-control" value="1">
+                                <button type="submit" name="submit" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></button>
+                                <input type="hidden" name="termekID" value="">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/tempcoin.jpg" alt="#" style="min-height: 25vh; max-height: 25vh ;">
+                    <div class="card-body">
+                        <div style="min-height: 20vh; max-height: 20vh;"  >    
+                            <h5 class="card-title"></h5>
+                            <p class="blockquote">2000  insert currency name </p>
+                            <p class="blockquote-footer">Buy 2000 insert currency name  here for only 20$ and get 500 insert currency name extra</p>
+                        </div>
+                        <form action="includes/kosar.php" method="POST">
+                            <div class="input-group">
+                                <input type="number" name="darabszam" id="darabszam_input" class="form-control" value="1">
+                                <button type="submit" name="submit" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></button>
+                                <input type="hidden" name="termekID" value="">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="img/tempcoin.jpg" alt="#" style="min-height: 25vh; max-height: 25vh ;">
+                    <div class="card-body">
+                        <div style="min-height: 20vh; max-height: 20vh;"  >    
+                            <h5 class="card-title"></h5>
+                            <p class="blockquote">5000  insert currency name </p>
+                            <p class="blockquote-footer">Buy 5000 insert currency name  here for only 50$ and get 1500 insert currency name extra</p>
+                        </div>
+                        <form action="includes/kosar.php" method="POST">
+                            <div class="input-group">
+                                <input type="number" name="darabszam" id="darabszam_input" class="form-control" value="1">
+                                <button type="submit" name="submit" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></button>
+                                <input type="hidden" name="termekID" value="">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+    </div> <!-- Első sor vége -->
   </div>
 </body>
 
