faa19a8a3f7080364526ea0321bb49b84e0b8f75
godoistvan <godopisti@gmail.com>
2021-11-21 14:47:48 +0100

    Added  items to the shop

--- shop.php
+++ shop.php
@@ -1,15 +1,48 @@
 <?php
-  require_once 'includes/navbarheader.php';
+require_once 'includes/navbarheader.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">500  insert currency name </h5>
+            <p class="card-text">Buy 500 insert currency name  here for only 5$ and get 30 insert currency name extra</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">1000  insert currency name</h5>
+          <p class="card-text">Buy 1000 insert currency name  here for only 10$ and get 100 insert currency name extra.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">1000  insert currency name</h5>
+    <p class="card-text">Buy 1000 insert currency name  here for only 10$ and get 100 insert currency name extra.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    </div>
+  </div>
 </body>
+
 </html>
