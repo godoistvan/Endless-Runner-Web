0a0338f7d7cd829b3859306666805c732d90d159
godoistvan <godopisti@gmail.com>
2021-11-25 20:45:45 +0100

    Added one placeholder job
    Added one placeholder job to see how the format will look like

--- apply.php
+++ apply.php
@@ -1,15 +1,42 @@
 <?php
-  require_once 'includes/navbarheader.php';
+require_once 'includes/navbarheader.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    
+  <div class="container">
+    <div class="row" style="min-height: 25px;"></div>
+
+
+    <div class="row">
+
+
+      <div class="col-3">
+        <div class="card">
+          <img src="img/placeholder.jpg" alt="#" style="min-height: 25vh; max-height: 25vh ;">
+          <div class="card-body">
+            <div style="min-height: 20vh; max-height: 20vh;">
+              <h5 class="card-title"></h5>
+              <p class="blockquote">Software engineer</p>
+              <p class="blockquote-footer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat provident repellendus sed rerum. Odio, animi!</p>
+              <button type="submit" class="btn btn-primary">Im interested</button>
+            </div>
+            
+          </div>
+          
+        </div>
+
+      </div>
+
+    </div>
 </body>
+
 </html>
