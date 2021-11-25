edd2e1cbf923cb062396041f0730a6359bf6dd36
godoistvan <godopisti@gmail.com>
2021-11-25 20:36:16 +0100

    Beta version of leaderboard
    Leaderboard is now functioning i will make it good later on in development

--- leaderboard.php
+++ leaderboard.php
@@ -10,6 +10,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    require_once("includes/database.php");
+    $sql="SELECT `userName`,`userHighscore`, `userRank`, `userElo` FROM `users` WHERE 1";
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+      echo '<table class="table col-md-7"><tr><th>Username</th><th>UserHighscore</th><th>UserRank</th><th>UserElo</th></tr>';
+      // output data of each row
+      while ($row = $result->fetch_assoc()) {
+          echo "<tr><td>" . $row["userName"] . "</td><td>" . $row["userHighscore"] . " </td><td>" . $row["userRank"] . "</td><td>" . $row["userElo"] . "</tr>";
+      }
+      echo "</table>";
+  } else {
+      echo "0 results";
+  }
+    ?>
 </body>
 </html>
