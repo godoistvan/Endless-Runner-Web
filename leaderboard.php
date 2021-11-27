1e581e8fee9be7012ecd14c0ccf07ac3e004ecb5
godoistvan <godopisti@gmail.com>
2021-11-27 21:33:42 +0100

    Fixed table size
    Previously the table couldn't properly fit the output

--- leaderboard.php
+++ leaderboard.php
@@ -15,7 +15,7 @@ require_once 'includes/navbarheader.php';
     <div class="mask d-flex align-items-center h-100 gradient-custom-3">
         <div class="container h-100">
             <div class="row d-flex justify-content-center align-items-center h-50">
-                <div class="col-4 col-md-4 col-lg-4 col-xl-4">
+                <div class="col-4 col-md-4 col-lg-4 col-xl-5">
                     <div class="card" style="border-radius: 15px;">
                         <div class="card-body p-2">
                             <?php
